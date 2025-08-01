c5ff881bd67e5ce0642f6974531aa38e98603420
Vedant-1509 <vedantmore1509@gmail.com>
2025-08-01 19:49:50 +0530

    all in one repo

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,23 +5,6 @@ const User = require("../models/User");
 
 const router = express.Router();
 
-// Signup
-// router.post("/signup", async (req, res) => {
-//   const { name, email, password } = req.body;
-//   try {
-//     let user = await User.findOne({ email });
-//     if (user) return res.status(400).json({ message: "User already exists" });
-
-//     const hashedPassword = await bcrypt.hash(password, 10);
-//     user = new User({ name, email, password: hashedPassword });
-//     await user.save();
-
-//     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
-//     res.status(201).json({ token, user: { name: user.name, email: user.email } });
-//   } catch (err) {
-//     res.status(500).json({ message: "Server error", error: err.message });
-//   }
-// });
 
 router.post("/signup", async (req, res) => {
   const { name, email, password, accountType, businessName } = req.body;
