22bb3c088ecdea33c35b8408a3bcbd624052e4b5
Vedant-1509 <vedantmore1509@gmail.com>
2025-08-16 19:53:31 +0530

    changed the springboot url

--- restapi/src/main/java/intershipapproach2/restapi/UI/WebConfig.java
+++ restapi/src/main/java/intershipapproach2/restapi/UI/WebConfig.java
@@ -8,7 +8,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // your React dev server
+                .allowedOrigins("http://ec2-16-170-249-229.eu-north-1.compute.amazonaws.com",
+                        "http://16.170.249.229:8080") // your React dev server
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
